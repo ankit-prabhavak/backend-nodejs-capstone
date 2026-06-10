ff6a303143dc23dba2ab802ac67529887e9ae5b1
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-06-10 23:56:19 +0530

    Fix React Router routes

--- secondchancewebsite/index.js
+++ secondchancewebsite/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+// React Router fallback
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/app', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.listen(process.env.PORT || 9000, () => {
+  console.log('Frontend server running');
 });
-
-app.listen(9000);
