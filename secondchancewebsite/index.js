36d3c706396eff138fd8e3ea105092afdc973786
ankit-prabhavak <ankitabcd1718@gmail.com>
2026-06-11 07:40:09 +0530

    Fix Express static serving for React build

--- secondchancewebsite/index.js
+++ secondchancewebsite/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
+const buildPath = path.join(__dirname, 'build')
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(buildPath))
 
-// React Router fallback
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
 
 app.listen(process.env.PORT || 9000, () => {
-  console.log('Frontend server running');
-});
+  console.log('Frontend server running')
+})
